3a97a67094d604b83afe3667f8feadd9af4195d7
abhirajdighe <digheabhirajr262@gmail.com>
2025-09-30 23:10:28 +0530

    Added screens

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,86 @@
+import {TextInput,View,Text, StyleSheet, Alert} from 'react-native'
+import PrimaryButton from '../components/PrimaryButton'
+import { useState } from 'react'
+
+function StartGameScreen({onPickNumber}) {
+
+    const [enteredNumber,setEnteredNumber] = useState('');
+
+    function numberInputHandler(enteredText){
+        setEnteredNumber(enteredText);
+    }
+
+    function resetInputHandler(){
+        setEnteredNumber('');
+    }
+
+    function confirmInputHandler(){
+        const choseNumber = parseInt(enteredNumber);
+
+        if(isNaN(choseNumber) || choseNumber<=0 || choseNumber>99){
+           // show alert 
+           Alert.alert('Invalid number',
+            'Number has to be a number between 1 and 99.',
+            [{text:'Okay',style:'destructive',onPress:resetInputHandler}]
+           )
+           return;
+        }
+
+        onPickNumber(choseNumber);
+    }
+
+  return (
+    <View style={styles.inputContainer}>
+        <TextInput 
+            style={styles.numberInput} 
+            maxLength={2} 
+            keyboardType='number-pad' 
+            autoCapitalize='none'
+            autoCorrect={false}
+            onChangeText={numberInputHandler}
+            value={enteredNumber}
+        />
+        <View style={styles.buttonsContainer}>
+            <View style={styles.buttonContainer}>
+                <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+            </View>
+            <View style={styles.buttonContainer}>
+                <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+            </View>
+        </View>
+    </View>
+  )
+}
+
+export default StartGameScreen
+
+const styles = StyleSheet.create({
+    inputContainer:{
+        justifyContent:'center',
+        alignItems:'center',
+        padding:8,
+        marginTop:100,
+        marginHorizontal:24,
+        backgroundColor:'#36051eff',
+        borderRadius:16,
+        elevation:4,
+    },
+    numberInput:{
+        height:60,
+        width:50,
+        fontSize:32,
+        borderBottomColor:'#ddb52f',
+        borderBottomWidth:2,
+        color:'#ddb52f',
+        marginVertical: 8,
+        fontWeight:'bold',
+        textAlign:'center',
+    },
+    buttonsContainer:{
+        flexDirection:'row',
+
+    },
+    buttonContainer:{
+        color:'white'
+    }
+})
